1a7b5c0edf9e2a839fe7c526aa602e7fa3fe9df0
Krista Brock <kb9286@gmail.com>
2021-05-02 21:54:33 -0500

    init

--- client/index.js
+++ client/index.js
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="eng">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="cc-logger" />
-    <meta name="keywords" content="mvp" />
+import App from './components/App.jsx'
 
-    <title>CC-Logger</title>
-
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-    <div id="app"></div>
-    <script src="bundle.js"></script>
-  </body>
-</html>
+ReactDOM.render(<App />, document.getElementById('app'))
